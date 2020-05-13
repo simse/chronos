420b20ad2b70393375e091d0ec82e5c7cfff163b
Simon Sørensen <hello@simse.io>
2020-05-13 22:03:32 +0200

    Update prebuild.sh

--- .circleci/prebuild.sh
+++ .circleci/prebuild.sh
@@ -1,9 +1,11 @@
-if [[ $CIRCLE_BRANCH = "edge" ]]
+if [[ $CIRCLE_BRANCH == "edge" ]]
 then
     export TAG=edge
 fi
 
-if [[ $CIRCLE_BRANCH = "master" ]]
+if [[ $CIRCLE_BRANCH == "master" ]]
 then
     export TAG=latest
 fi
+
+echo $TAG
