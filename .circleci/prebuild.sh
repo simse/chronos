27c9637d0cab875c1c521ba76dcf1eded37bb92f
Simon Sørensen <hello@simse.io>
2020-05-14 20:48:18 +0200

    Docker images are now built for all branches

--- .circleci/prebuild.sh
+++ .circleci/prebuild.sh
@@ -1,11 +1,8 @@
-if [ $CIRCLE_BRANCH = "edge" ]
-then
-    echo 'export TAG=edge' >> $BASH_ENV
-fi
-
 if [ $CIRCLE_BRANCH = "master" ]
 then
     echo 'export TAG=master' >> $BASH_ENV
+else
+    echo 'export TAG={$CIRCLE_BRANCH}' >> $BASH_ENV
 fi
 
 echo $TAG
