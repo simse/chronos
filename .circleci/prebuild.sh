6ef40687e09d11bcf2c60b66bed31085dbd2db6b
Simon Sørensen <hello@simse.io>
2020-05-14 21:02:23 +0200

    Fixed curly brackets in prebuild script

--- .circleci/prebuild.sh
+++ .circleci/prebuild.sh
@@ -2,7 +2,7 @@ if [ $CIRCLE_BRANCH = "master" ]
 then
     echo 'export TAG=master' >> $BASH_ENV
 else
-    echo 'export TAG={$CIRCLE_BRANCH}' >> $BASH_ENV
+    echo 'export TAG=$CIRCLE_BRANCH' >> $BASH_ENV
 fi
 
 echo $TAG
