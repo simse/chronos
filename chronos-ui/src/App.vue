dc463cec9d1aa8ae73b6e8abfb478e2ea39c79de
Simon Sørensen <hello@simse.io>
2020-05-16 03:56:28 +0200

    Restarted UI from scratch

--- chronos-ui/src/App.vue
+++ chronos-ui/src/App.vue
@@ -1,116 +1,43 @@
 <template>
   <div id="app">
-    <link rel="stylesheet" href="https://cdn.materialdesignicons.com/2.5.94/css/materialdesignicons.min.css">
-    <link href="https://fonts.googleapis.com/css2?family=Inter:wght@300;400;700;900&display=swap" rel="stylesheet">
-    <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@300;400;700;900&display=swap" rel="stylesheet">
-
-    <!--h1 class="is-size-1 title">Chronos</h1>
-    <p class="is-size-5 subtitle">{{ $t('version') | capitalize }}: {{ version }}</p-->
-
-    <Navbar />
-
-    <router-view/>
-
-    <!--div class="bottom-bar">
-      <div class="status">
-        <span>
-          <span :class="{'icon':true,'red':!connectedBool}"></span>
-          {{ $t(connected) }}
-        </span>
-      </div>
-
-      <div class="tasks">
-
-      </div>
-    </div-->
+    <div id="nav">
+      <router-link to="/">Home</router-link> |
+      <router-link to="/about">About</router-link>
+    </div>
+    <router-view />
   </div>
 </template>
 
-<script>
-import version from '!raw-loader!./VERSION'
-import Navbar from './components/Navbar.vue'
-
-export default {
-    name: 'App',
-    components: {
-      Navbar
-    },
-    data() {
-        return {
-            version: version
-        }
-    },
-    computed: {
-      connectedBool() {
-        return this.$store.state.$chronos.connected
-      },
-      connected() {
-        if(this.connectedBool) {
-          return 'connected'
-        } else {
-          return 'disconnected'
-        }
-      }
-    }
-}
-
-</script>
-
 <style lang="scss">
-*,
-*::before,
-*::after {
-    box-sizing: border-box;
-}
-
-html {
-    font-size: 100%;
+:root {
+  --grey: #1f1f1f;
+  --blue: #007aff;
 }
 
 body {
-  background: #f6f9fc;
-  min-height: 100vh;
-  font-family: 'Nunito' !important;
+  margin: 0;
+  box-sizing: border-box;
 }
 
-.title,
-.subtitle {
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
   text-align: center;
-}
-
-.title {
-    padding-bottom: 10px;
-}
-
-.subtitle {
-    padding-bottom: 45px;
-}
-
-.version {
-    opacity: 0.7;
-}
-
-.bottom-bar {
-  display: flex;
-  justify-content: space-between;
-  position: fixed;
-  bottom: 0;
-  left: 0;
-  width: 100vw;
-  padding: 12px 20px;
-  background: #000;
   color: #fff;
+  background: var(--grey);
+  min-height: 100vh;
+}
 
-  .icon {
-    width: 7px;
-    height: 7px;
-    background: #4cd137;
-    border-radius: 100%;
-    margin-bottom: 2px;
-    margin-right: 5px;
+#nav {
+  padding: 30px;
 
-    &.red {
-      background: #e84118;
+  a {
+    font-weight: bold;
+    color: #2c3e50;
+
+    &.router-link-exact-active {
+      color: #42b983;
     }
   }
 }
--- chronos-ui/src/main.js
+++ chronos-ui/src/main.js
@@ -1,91 +1,13 @@
-import Vue from 'vue'
-import Vuex from 'vuex'
+import Vue from "vue";
+import App from "./App.vue";
+import "./registerServiceWorker";
+import router from "./router";
+import store from "./store";
 
-import App from './App.vue'
-import router from './router'
-import { EventBus } from './bus.js'
-
-import Buefy from 'buefy'
-import 'buefy/dist/buefy.css'
-
-import axios from 'axios'
-import i18n from './i18n'
-import {
-  capitalize
-} from './filters/capitalize'
-
-Vue.prototype.$http = axios
-Vue.config.productionTip = false
-Vue.use(Buefy)
-Vue.use(Vuex)
-Vue.filter('capitalize', capitalize);
-
-if (Vue.config.devtools) {
-  Vue.prototype.api = 'http://localhost:5000/api'
-} else {
-  Vue.prototype.api = '/api'
-}
-
-
-const store = new Vuex.Store({
-  state: {
-    $chronos: {
-      connected: false
-    },
-    scripts: []
-  },
-  mutations: {
-    connected(state, payload) {
-      state.$chronos.connected = payload
-    },
-    scripts(state, payload) {
-      state.scripts = payload
-    }
-  }
-})
-
+Vue.config.productionTip = false;
 
 new Vue({
   router,
-  i18n,
   store,
-  render: h => h(App),
-  mounted() {
-    this.loadAllScripts();
-
-    EventBus.$on('reloadScripts', () => {
-      this.loadAllScripts();
-    })
-
-    EventBus.$on('addLoadingScript', (payload) => {
-      this.addLoadingScript(payload.name, payload.uid)
-    })
-
-    
-  },
-  methods: {
-    loadAllScripts() {
-      this.$http.get(this.api + '/scripts').then(response => {
-        store.commit('scripts', response.data)
-        store.commit('connected', true)
-
-        
-      }).catch(() => {
-        store.commit('connected', false)
-      })
-
-      
-    },
-    addLoadingScript(name, uid) {
-      let scripts = this.$store.state.scripts;
-      scripts.push({
-        name: name,
-        uid: uid,
-        loading: true,
-        logs: []
-      })
-
-      store.commit('scripts', scripts)
-    },
-  }
-}).$mount('#app')
+  render: h => h(App)
+}).$mount("#app");
